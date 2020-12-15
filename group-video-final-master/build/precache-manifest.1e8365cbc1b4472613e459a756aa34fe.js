358d01f4b25ccfd08772d63f64181d6e995fcf17
caahkaori <carolinetakano8@gmail.com>
2020-12-15 14:36:01 +0900

    testing webrtc

---
+++ group-video-final-master/build/precache-manifest.1e8365cbc1b4472613e459a756aa34fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333ac80e41e4607c8a52b8bf69278226",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4260d93242b6f3343e1a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "20a0ba9c0f3c521b578a",
+    "url": "/static/js/2.d8b64fd8.chunk.js"
+  },
+  {
+    "revision": "fb4cf9246b297684a2f67d970514db84",
+    "url": "/static/js/2.d8b64fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4260d93242b6f3343e1a",
+    "url": "/static/js/main.56040e96.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
